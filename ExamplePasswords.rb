63353493068e5911b5f80c895b6e3ffc465c1224
Joel Pearson <virtuosojoel@gmail.com>
2014-09-04 14:15:45 +0100

    As if I'd put real passwords on the open internet

---
+++ ExamplePasswords.rb
@@ -0,0 +1,28 @@
+module Passwords
+  EmailServer = 'servername'
+  EmailSuffix = '@domain.com'
+  EmailDomain = 'domain'
+  EmailFrom = 'noreply@example.com'
+  MyName = 'firstname lastname'
+  ErrorAlert = 'firstname lastname'
+  EncryptionKey = 'zxcv'
+
+  module SQLRoleCall
+    UserName = 'username'
+    Password = 'password'
+    DSN = 'dsn'
+  end
+  
+  module SQLMitel
+    UserName = 'username'
+    Password = 'password'
+    DSN = 'dsn'
+  end
+  
+  module SQLAlarmMaster
+    UserName = 'username'
+    Password = 'password'
+    DSN = 'dsn'
+  end
+  
+end
